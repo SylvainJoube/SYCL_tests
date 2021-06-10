3833c031ab66745a334a7621475aa2eb15040bfe
Sylvain Joube <sylvain.joube@gmail.com>
2021-06-10 17:28:06 +0200

    hello world

---
+++ mem_bench/memory_benchmark.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+// SyCL specific includes
+#include <CL/sycl.hpp>
+#include <array>
+#include <sys/time.h>
+
+int main(int argc, char *argv[])
+{
+    // Not sure I'll need that
+    /*if (argc < 2){
+        std::cout << "Not enough arguments, minimum requirement: " << std::endl;
+        std::cout << "./exe <data_path>" << std::endl;
+        return -1;
+    }
+    auto data_path = std::string(argv[1]);*/
+
+    std::cout << "SYCL memory benchmark." << std::endl;
+
+
+}
